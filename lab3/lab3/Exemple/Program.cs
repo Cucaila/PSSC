896b3bbbe751116aa315eb711fe90e4cbc461323
Adina <adina.cucaila@student.upt.ro>
2023-10-19 22:34:12 +0300

    almost done

---
+++ lab3/lab3/Exemple/Program.cs
@@ -0,0 +1,58 @@
+﻿using lab3;
+using lab3.Commands;
+using lab3.Models;
+using lab3.Workflow;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var products = ReadListOfProducts().ToArray();
+        PayShoppingCartCommands command = new(products);
+        PayShoppingCartWorkflow workflow = new();
+        var result = workflow.Execute(command, (productName) => true);
+
+        result.Match(
+            whenShoppingCartPayFailedEvent: @event =>
+            {
+                Console.WriteLine($"Pay failed: {@event.Reason}");
+                return @event;
+            },
+            whenShoppingCartPaySuccededEvent: @event =>
+            {
+                Console.WriteLine($"Pay succeded: {@event.Total}");
+                return @event;
+            }
+            );
+
+        Console.WriteLine("Done");
+    }
+
+    private static List<UnvalidatedShoppingCartProduct> ReadListOfProducts()
+    {
+        List<UnvalidatedShoppingCartProduct> listOfProducts = new();
+        do
+        {
+            var productName = ReadValue("Product name: ");
+            if (string.IsNullOrEmpty(productName))
+            {
+                break;
+            }
+
+            var price = ReadValue("Price: ");
+            if (string.IsNullOrEmpty(price))
+            {
+                break;
+            }
+
+            listOfProducts.Add(new(productName, price));
+        } while (true);
+        return listOfProducts;
+    }
+
+    private static string? ReadValue(string prompt)
+    {
+        Console.Write(prompt);
+        return Console.ReadLine();
+    }
+}
---
+++ lab3/lab3/Models/ShoppingCartPayedEvent.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharp.Choices;
+
+namespace lab3.Models
+{
+    [AsChoice]
+    public static partial class ShoppingCartPayedEvent
+    {
+        public interface IShoppingCartPayedEvent { }
+
+        public record ShoppingCartPaySuccededEvent : IShoppingCartPayedEvent
+        {
+            public decimal Total { get; }
+
+            public ShoppingCartPaySuccededEvent(decimal total)
+            {
+                Total = total;
+            }
+        }
+
+        public record ShoppingCartPayFailedEvent : IShoppingCartPayedEvent
+        {
+            public string Reason { get; }
+
+            public ShoppingCartPayFailedEvent(string reason)
+            {
+                Reason = reason;
+            }
+        }
+    }
+}
